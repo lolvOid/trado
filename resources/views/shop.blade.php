ae683b55b3b57574673e1a77a03393df762a2c74
kiyokata <42716777+kiyokata@users.noreply.github.com>
2018-08-30 02:52:15 +0630

    Add files via upload

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,153 +1,307 @@
 @extends('master')
 @section('pages')
-    <section class="clean-block clean-catalog dark">
-        <div class="container " style="background-color:#ffffff;">
+
+<!--Product Slider-->
+<section class="clean-block clean-catalog dark">
+        <div class="container" style="background-color:#ffffff;">
             <div class="block-heading">
                 <h2 class="text-info" style="font-size:35px;">SHOP CATEGORY</h2>
                 <p></p>
             </div>
-            <div class="content">
+            <section id="filtr-gallery" style="background-color:rgb(255,255,255);">
+                <div class="container">
+					<h1 class="text-center"></h1>
+					<!--Category Start-->
+					<div class="filtr-controls">
+						<span class="active" data-filter="all">all</span>
+	<span data-filter="1">category 1</span>
+	<span data-filter="2">category 2</span>
+    <span data-filter="3">category 3</span>
+    <span data-filter="4">category 4</span>
+<span data-filter="5">category 5</span>
+    <span data-filter="6">category 6</span>
+<span data-filter="7">category 7</span>
+    <span data-filter="8">category 8</span>
+    <span data-filter="9">category 9</span>
+    <span data-filter="10">category 10</span>
+    
+</div>
+<!--Category ENd-->
 
-				<div class="row " style="background-color:#ffffff;">
-				<div class="col-md-3 navbar-expand-md border border-light " style="background-color:#ffffff;">
-				<div class=" col-md-3">
-     <div class="d-none    d-md-block">
-        <div class=" filters">
-            <h4>Category</h4>
-            <ul class="nav navbar-nav"><li class="nav-item" role="presentation"><a class="nav-link " href="shop">All</a></li>
-				@foreach ($categories as $category)
+<div class="filter">
+    <form>
+        <input placeholder="Brand Name,Specification,etc "><br>
+       
+    <label for="type">Product Type</label>
+    <select id="type">
+             <option value="Laptop">Laptop</option>
+             
+        </select>
+  
+    <label for="price1">Price</label>
+    
+    <select id="price1">
+            
+        <option value="High">High</option>
+         <option value="Low">Low</option>
+        </select>
+    
+    
+    <div style="margin-top:20px;">
+        <label >Featured</label>
+   <label  id="togglebutton" class="switch">
+    .<input class="switch" type="checkbox">
+  <span class="slider round"></span>
+</label>
+    </div>
+    
+    </form>
 
-				
-					<li class="nav-item " role="presentation"><a class="nav-link " href="{{route('shop.index',['category'=>$category->slug	])}}">{{$category->name}}</a></li>
-				@endforeach
-                {{-- <li class="nav-item" role="presentation"><a class="nav-link " href="index.html">Phone</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="shop.html">Laptop</a></li>
-				<li class="nav-item" role="presentation"><a class="nav-link" href="signup.html">Desktop</a></li> --}}
-				
-            </ul>
+</div>
+<!--Product Row Start-->
+<div class="row filtr-container">
+   <!--Product Start-->
+<div class="col-12 col-sm-10 col-md-3 col-lg-3 col-xl-4 offset-sm-0 col-md-12 card filtr-item"  data-category="3" >
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-xl-12 offset-sm-0 col-md-5">
+            <div style="background-color:#ffffff;width:200px;"></div><img src="img/iPhoneX.png" style="height:242px;" /></div>
+        <div class="col-sm-10 col-md-12 col-lg-12 col-xl-12 offset-sm-9 offset-lg-0 offset-xl-0 offset-md-1 col-md-6" style="margin-left:0px;padding-left:21px;">
+            <h1 class="product-text-color">CitriWist</h1><small style="font-style:oblique;">Citrus Reamer</small>
+            <h5>In Stock</h5>
+            <div class="start-icon"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i>
+                <h6 class="product-price">$ 9.99<small>USD</small></h6>
+            </div><small class="product-review">3 - 272 reviews</small>
+            <div>
+                <h3 class="product-description">CitriTwist is no ordinary reamer. It extracts every last drop of citrus juice without making a big ol&#39; mess. How, you ask? Its spiraled form allows the juice to drip down the inside walls, not your hands. Genius!</h3>
+        
+            <button class="btn btn-primary pull-right" type="button"><i class="fa fa-shopping-cart" style="color:#fff;"></i>&nbsp;Buy Now</button></div>
         </div>
     </div>
-    <div class="d-md-none"><a class="btn btn-link d-md-none filter-collapse" data-toggle="collapse" aria-expanded="false" aria-controls="filters" role="button" href="#filters">Categories<i class="icon-arrow-down filter-caret"></i></a>
-        <div class="collapse" id="filters">
-            <ul class="nav navbar-nav">
-				@foreach ($categories as $category)
-					<li class="nav-item" role="presentation"><a class="nav-link " href="{{route('shop.index',['category'=>$category->slug	])}}">{{$category->name}}</a></li>
-				@endforeach
-                {{-- <li class="nav-item" role="presentation"><a class="nav-link " href="index.html">Phone</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="shop.html">Laptop</a></li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="signup.html">Desktop</a></li> --}}
-            </ul>
+</div><!--Product End-->
+<!--DATA CATEGORY IS FOR THE PRODUCT CATEGORY A TAG-->
+	
+</div></div>
+			</section>
+			<!--Product Section End-->
+            <div class="content"></div>
         </div>
-    </div>
-</div>
-</div>
+	</section>
+	
 
-					<div class="col border border-light" style="background-color:#ffffff;">
-					
-					
-
-					<div class="container">
-						<!--Product Row-->
-
-		
-        <div class="row">
-			<!--Product Column-->
-			@forelse ($products as $product)
-       		<div class="col-sm-4">
-                <div  class="card">
-                    <img  style="width:100%;height:170px;"class="card-img-top img-fluid	" src="{{'storage/'.$product->image}}">
-                    	<div class="card-block">
-                        <h4 class="card-title">{{$product->name}}</h4>
-                        <div class="meta">
-                            <a href="#">Seller-</a>
-                        </div>	
-                        <div class="card-text">
-                           <p>{{$product->presentPrice()}}</p>
-                        </div>
-                    </div>
-                    <div class="card-footer">
-					<a href="{{route('shop.show',$product->slug)}}"><button style="background-color:#5CABFF; border-color:#93c7ff;"class="btn btn-secondary float-right btn-sm">show</button>
-					</a>
-                    </div>
-                </div>
-			</div><!--PRoduct COlumn end-->
-			@empty
-			<div style="font-size:2em;padding-left:23.5%">No items found</div>
-		
-        </div><!--Product Row end-->
-		@endforelse	
-               
-                        <nav class="align-content-end">
-                            <ul class="pagination">
-                                <li class="page-item"><a class="page-link" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
-                                <li class="page-item"><a class="page-link">1</a></li>
-                                <li class="page-item"><a class="page-link">2</a></li>
-                                <li class="page-item"><a class="page-link">3</a></li>
-                                <li class="page-item"><a class="page-link">4</a></li>
-                                <li class="page-item"><a class="page-link">5</a></li>
-                                <li class="page-item"><a class="page-link" aria-label="Next"><span aria-hidden="true">»</span></a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+	<!--Pagination -->
     <div style="background-color:#ffffff;">
         <div class="container">
-            <div class="row">
-                <div class="col">
-					<div class="container">
+            <nav class="align-content-end">
+                <ul class="pagination center-block">
+                    <li class="page-item"><a class="page-link" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                    <li class="page-item"><a class="page-link">1</a></li>
+                    <li class="page-item"><a class="page-link">2</a></li>
+                    <li class="page-item"><a class="page-link">3</a></li>
+                    <li class="page-item"><a class="page-link">4</a></li>
+                    <li class="page-item"><a class="page-link">5</a></li>
+                    <li class="page-item"><a class="page-link" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                </ul>
+            </nav>
+		</div>
+		
 
-				
-					<div class="row ">
-						<div class="col-md-12 border border-light ">
-							<h2>Trending <b>Products</b></h2>
-							<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
-							<!-- Carousel indicators -->
-						<!--	<ol class="carousel-indicators">
-								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-								<li data-target="#myCarousel" data-slide-to="1"></li>
-								
-							</ol> -->  
-							<!-- Wrapper for carousel items -->
-							<div class="carousel-inner">
-									<div class="item carousel-item active">
-										<div class="row">
-											
-												
-										@foreach ($products as $item)
-											<!--Product Item-->
-											<div class="col-md-4">
-												<div class="thumb-wrapper">
-													<div class="img-box">
-														<img src="{{'/storage/'.$item->image}}" class="img-responsive img-fluid" alt="">
-													</div>
-													<div class="thumb-content">
-														<h4>{{$item->name}}</h4>
-														<p class="item-price"><strike>{{$item->presentPrice()}}</strike> <span>{{discountPrice($item->price,10)}}</span></p>
-														
-														<a href="{{route('shop.show',$product->slug)}}" class="btn btn-primary"><i class="fa fa-cart-plus" style="font-size:20px"></i></a>
-													</div>						
-													</div>
-											</div>
-										
-											<!--Product Item end-->
-											@endforeach
-										</div>	
-									</div>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col"><div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<h2>Trending <b>Products</b></h2>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
+			<!-- Carousel indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>   
+			<!-- Wrapper for carousel items -->
+			<div class="carousel-inner">
+				<div class="item carousel-item active">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/ipad.jpg" class="img-responsive img-fluid" alt="">
 								</div>
-							
-							
-							<!-- Carousel controls -->
-									<!--<a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
-										<i class="fa fa-angle-left"></i>
-									</a>
-									<a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
-										<i class="fa fa-angle-right"></i>
-								</a>-->
+								<div class="thumb-content">
+									<h4>Apple iPad</h4>
+									<p class="item-price"><strike>$400.00</strike> <span>$369.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
 							</div>
 						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/headphone.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Sony Headphone</h4>
+									<p class="item-price"><strike>$25.00</strike> <span>$23.99</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>		
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/macbook-air.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Macbook Air</h4>
+									<p class="item-price"><strike>$899.00</strike> <span>$649.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>								
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/nikon.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Nikon DSLR</h4>
+									<p class="item-price"><strike>$315.00</strike> <span>$250.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item carousel-item">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/play-station.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Sony Play Station</h4>
+									<p class="item-price"><strike>$289.00</strike> <span>$269.00</span></p>
+								
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/macbook-pro.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Macbook Pro</h4>
+									<p class="item-price"><strike>$1099.00</strike> <span>$869.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/speaker.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Bose Speaker</h4>
+									<p class="item-price"><strike>$109.00</strike> <span>$99.00</span></p>
+								
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/galaxy.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Samsung Galaxy S8</h4>
+									<p class="item-price"><strike>$599.00</strike> <span>$569.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>						
+					</div>
+				</div>
+				<div class="item carousel-item">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/iphone.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Apple iPhone</h4>
+									<p class="item-price"><strike>$369.00</strike> <span>$349.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/canon.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Canon DSLR</h4>
+									<p class="item-price"><strike>$315.00</strike> <span>$250.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/pixel.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Google Pixel</h4>
+									<p class="item-price"><strike>$450.00</strike> <span>$418.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>	
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/watch.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>Apple Watch</h4>
+									<p class="item-price"><strike>$350.00</strike> <span>$330.00</span></p>
+									
+									<a href="#" class="btn btn-primary">Add to Cart</a>
+								</div>						
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+              
+              
+			<!-- Carousel controls -->
+			<a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+				<i class="fa fa-angle-left"></i>
+			</a>
+			<a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+				<i class="fa fa-angle-right"></i>
+			</a>
+		</div>
+		</div>
 	</div>
 </div></div>
             </div>
