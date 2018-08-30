2f669e9744346768eed8968b034106f435e6321c
unknown <stm1998sithumyo@gmail.com>
2018-08-31 06:01:47 +0630

    ''

--- app/Http/Controllers/ProductsDataController.php
+++ app/Http/Controllers/ProductsDataController.php
@@ -133,7 +133,7 @@ class ProductsDataController extends Controller
         $product->slug = $request->input('slug');
         $product->details = $request->input('details');
         $product->price = $request->input('price') * 100;
-        $product->description = $request->input('description');
+        $product->description = wordwrap($request->input('description'),75,"<br />\n");
         $product->featured = $featured;
         if($request->hasFile('product_img')){
             $product_img = $request->file('product_img');
