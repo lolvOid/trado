98e8b377ae174267314e0c08032fb54f33148784
unknown <stm1998sithumyo@gmail.com>
2018-08-31 05:28:46 +0630

    ''

--- resources/views/productform.blade.php
+++ resources/views/productform.blade.php
@@ -91,6 +91,20 @@
                                     $('input#description').val($descriptionValue);
                                     $("#editForm").submit();                
                                 }
+                                $("#summernote").summernote({
+                                    height: 300,
+                                    toolbar: [
+                                        [ 'style', [ 'style' ] ],
+                                        [ 'font', [ 'bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear'] ],
+                                        [ 'fontname', [ 'fontname' ] ],
+                                        [ 'fontsize', [ 'fontsize' ] ],
+                                        [ 'color', [ 'color' ] ],
+                                        [ 'para', [ 'ol', 'ul', 'paragraph', 'height' ] ],
+                                        [ 'table', [ 'table' ] ],
+                                        [ 'insert', [ 'link'] ],
+                                        [ 'view', [ 'undo', 'redo', 'fullscreen', 'codeview', 'help' ] ]
+                                    ]
+                                });
                                 
                             </script>
         @endsection()
