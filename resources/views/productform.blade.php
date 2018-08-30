d0363438a21193d44e9be2c8fd806b149b69c4fb
unknown <stm1998sithumyo@gmail.com>
2018-08-31 04:37:03 +0630

    ''

--- resources/views/productform.blade.php
+++ resources/views/productform.blade.php
@@ -76,7 +76,7 @@
                                                         <div class="summernote"></div>
                                                     </div>
                                                 </div>
-                                                <button onclick="beforeSubmit();" type="submit" name="edit-save" id="edit-save" class="btn btn-success pull-right">Save</button>
+                                                <button onclick="beforeSubmit();" name="edit-save" id="edit-save" class="btn btn-success pull-right">Save</button>
                                             </form>
                                         </div>
                                     </div>
