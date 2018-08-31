d0bf84877412ef02437bd1eecff62b46aaeb6380
void <linhtet1998itachi@gmail.com>
2018-08-31 09:04:22 +0630

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@
 use Illuminate\Support\Facades\Request;
 use App\Product;
 use App\User;
-
+use Blockchain;
 function productImage($path){
      return file_exists('storage/'.$path)?('storage/'.$path):('images/not found');
 }
@@ -30,4 +30,31 @@ function cleanProducts(){
             $product->delete();
         }
     }
+}
+function enableHD($guid,$wallet_password)
+{
+    $local_url = "http://localhost:3000";
+    $url = "$local_url/merchant/$guid/enableHD?password".http_build_query([$wallet_password]);
+}
+
+function ChartData(){
+
+    return BlockChain::GetStats();
+    
+}
+function getChart($chartName,$timespan,$rollingAverage,$start,$format,$sampled)
+{
+
+    $base_url = "https://api.blockchain.info";
+
+    $params = [
+        'charts'=>$chartName,
+        'timespan' =>$timeSpan,
+        'rollingAverage'=>$rollingAverage,
+        'start'=>$start,
+        'format'=>$format,
+        'sampled'
+    ];
+    $url = $base_url.http_build_query($params);
+    dd($url);
 }
