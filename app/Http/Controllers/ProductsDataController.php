52d0df0957fc65590a92f62362217d3f26d9e82e
unknown <stm1998sithumyo@gmail.com>
2018-08-31 06:39:23 +0630

    ''

--- app/Http/Controllers/ProductsDataController.php
+++ app/Http/Controllers/ProductsDataController.php
@@ -127,7 +127,7 @@ class ProductsDataController extends Controller
        }else{
            $featured = 0;
        }
-    //    dd($request);
+       dd($request);
         $product = Product::find($request->input('id'));
         $product->name = $request->input('name');
         $product->slug = $request->input('slug');
