fe35bdc6d449a6451866f4a4175ffa8cac650278
unknown <stm1998sithumyo@gmail.com>
2018-09-02 22:25:27 +0630

    ''

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -34,27 +34,6 @@
 		
 	</div>
 </form>
-{{-- @foreach ($comments as $comment)
-<p>{{User::where('id',$comment->user_id)->first()->name}}</p><br/>
-<p>{{$comment->comment}}</p><br>
-@endforeach
-
-
-
-
-<form method="POST" action="{{route('comment.store')}}" >
-	@foreach ($comments as $comment)
-	{{-- <p>{{User::where('id',$comment->user_id)->first()->name}}</p><br/>
-	<p>{{$comment->comment}}</p><br>
-	@endforeach
-	{{ csrf_field() }}
-	<div class="form-group">
-		<label for="comment">Comment:</label>
-		<input type="hidden" name="product_id" value="{{$product->id}}">
-		<input type="hidden" name="owner_id" value="{{$owner->id}}">
-		<input type="text" class="form-control" rows="5" name="comment" id="comment">
-	</div>
-</form> --}}
 </div>
 </div>
 
@@ -97,7 +76,7 @@
     <div class="comment">
       <!-- Comment Avatar -->
       <div class="comment-avatar">
-        <img src="http://gravatar.com/avatar/412c0b0ec99008245d902e6ed0b264ee?s=80">
+        <img src="{{User::where('id',comment->cmt_user_id)->first()->avatar}}">
       </div>
 
       <!-- Comment Box -->
@@ -106,121 +85,16 @@
         <div class="comment-footer">
           <div class="comment-info">
             <span class="comment-author">
-              <a href="">Sexar</a>
+              <a href="">{{User::where('id',comment->cmt_user_id)->first()->name}}</a>
             </span>
-
-          </div>
-
-          <div class="comment-actions">
-            <a href="#">Reply</a>
           </div>
         </div>
       </div>
     </div>
     @endforeach
-       {{-- <div class="comment">
-      <!-- Comment Avatar -->
-      <div class="comment-avatar">
-        <img src="http://gravatar.com/avatar/412c0b0ec99008245d902e6ed0b264ee?s=80">
-      </div>
-
-      <!-- Comment Box -->
-      <div class="comment-box">
-        <div class="comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto temporibus iste nostrum dolorem natus recusandae incidunt voluptatum.</div>
-        <div class="comment-footer">
-          <div class="comment-info">
-            <span class="comment-author">
-              <a href="">Sexar</a>
-            </span>
-
-          </div>
-
-          <div class="comment-actions">
-            <a href="#">Reply</a>
-          </div>
-        </div>
-      </div>
-    </div>
-      
-      
-    <!-- Comment - Dummy -->
-    <div class="comment">
-      <!-- Comment Avatar -->
-      <div class="comment-avatar">
-        <img src="http://lorempixel.com/200/200/people">
-      </div>
-
-      <!-- Comment Box -->
-      <div class="comment-box">
-        <div class="comment-text">Eligendi voluptatum ducimus architecto tempore, quaerat explicabo veniam fuga corporis totam reprehenderit quasi sapiente modi tempora at perspiciatis mollitia, dolores voluptate. Cumque, corrupti?</div>
-        <div class="comment-footer">
-          <div class="comment-info">
-            <span class="comment-author">
-              <a href="mailto:ximme13@somedomain.io">Ximme</a>
-            </span>
-         
-          </div>
-
-          <div class="comment-actions">
-            <a href="#">Reply</a>
-          </div>
-        </div>
-      </div>
-    </div> --}}
   </div>
 </div>
 </div>
-<!--Comment Section End-->
-
-	<!--Related Products-->
-    {{-- <div class="container" style="background-color:#ffffff;">
-        <div class="row">
-            <div class="col">
-				<div class="row">
-					<div class="col-md-12 border border-light ">
-			<h2>Related <b>Products</b></h2>
-			<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
-			<!-- Carousel indicators -->
-			<ol class="carousel-indicators">
-				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#myCarousel" data-slide-to="1"></li>
-				
-			</ol>   
-			<!-- Wrapper for carousel items -->
-			<div class="carousel-inner">
-				<div class="item carousel-item active">
-					<div class="row">
-						
-						<!--Product Item-->@foreach ($related as $item)	
-						<div class="col-sm-3">
-							
-							<div class="thumb-wrapper">
-									
-								<div class="img-box">
-									<img src="{{$item->images}}" class="img-responsive img-fluid" alt="">
-								</div>
-								<div class="thumb-content">
-									<h4>{{$item->name}}</h4>
-									<p class="item-price"><strike>{{$item->presentPrice()}}</strike> <span>{{discountPrice($item->price,10)}}</span></p>
-									
-									<a href="" class="btn btn-primary"><i class="fa fa-cart-plus" style="font-size:20px"></i></a>
-								</div>					
-								
-							</div>
-							@endforeach	
-						
-						</div>
-						
-						<!--Product Item end-->
-						
-					</div>
-				</div>
-			</div>
-			<!-- Carousel controls -->
-		
-		</div>
-		</div>
-	</div> --}}
 </div>
         </div>
     </div>
