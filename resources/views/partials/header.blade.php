881b8d21cd637d3f024c954b56c90f355bb8075b
unknown <stm1998sithumyo@gmail.com>
2018-09-03 03:59:46 +0630

    ''

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tradoexchange</title>
+    <title>Trado</title>
     <link rel="shortcut icon" href="img/tradoalpha.png" type="image/x-icon">
     <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }} ">
     <link rel="stylesheet" href="{{ URL::asset('fonts/font-awesome.min.css') }}">
