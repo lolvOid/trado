466ae79def65a0180ab0bc7fe18c61d0f6c478ce
kiyokata <42716777+kiyokata@users.noreply.github.com>
2018-09-02 18:19:52 +0630

    Update product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -38,6 +38,10 @@
 <p>{{User::where('id',$comment->user_id)->first()->name}}</p><br/>
 <p>{{$comment->comment}}</p><br>
 @endforeach
+
+
+
+
 <form method="POST" action="{{route('comment.store')}}" >
 	@foreach ($comments as $comment)
 	{{-- <p>{{User::where('id',$comment->user_id)->first()->name}}</p><br/>
@@ -55,7 +59,158 @@
 </div>
 
 
+<!--Comment Section-->
 
+<div class="col-md-12  col-sm-12">
+<div class="comments-app" ng-app="commentsApp">
+  <h1>comments</h1>
+  
+  <!-- From -->
+  <div class="comment-form">
+    <!-- Comment Avatar -->
+    <div class="comment-avatar">
+      <img src="http://lorempixel.com/200/200/people">
+    </div>
+
+    <form class="form" name="form" ng-submit="form.$valid && cmntCtrl.addComment()" novalidate>
+      <div class="form-row">
+        <input  type="text"               class="input"
+                  ng-model="cmntCtrl.comment.text"
+                  placeholder="Add comment..."
+                  required>
+      </div>
+
+      <div class=" col-md-12 col-sm-12 form-row">
+        <input
+               class="input"
+              
+               placeholder="Email"
+               type="email">
+      </div>
+
+      <div class="form-row text-right">
+        <input
+               id="comment-anonymous"
+               
+               type="checkbox">
+        <label for="comment-anonymous">Anonymous</label>
+      </div>
+
+      <div class="form-row">
+        <input type="submit" value="Add Comment">
+      </div>
+    </form>
+  </div>
+
+  <!-- Comments List -->
+  <div class="comments">
+    <!-- Comment -->
+    <div class="comment" ng-repeat="comment in cmntCtrl.comments | orderBy: '-date'">
+      <!-- Comment Avatar -->
+      <div class="comment-avatar">
+        <img ng-src="{{ comment.avatarSrc }}">
+      </div>
+
+      <!-- Comment Box -->
+      <div class="comment-box">
+        <div class="comment-text">{{ comment.text }}</div>
+        <div class="comment-footer">
+          <div class="comment-info">
+            <span class="comment-author">
+              <em ng-if="comment.anonymous">Anonymous</em>
+              <a ng-if="!comment.anonymous" href="{{ comment.author }}">{{ comment.author }}</a>
+            </span>
+           
+          </div>
+
+          <div class="comment-actions">
+            <a href="#">Reply</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Comment - Dummy -->
+    <div class="comment">
+      <!-- Comment Avatar -->
+      <div class="comment-avatar">
+        <img src="http://gravatar.com/avatar/412c0b0ec99008245d902e6ed0b264ee?s=80">
+      </div>
+
+      <!-- Comment Box -->
+      <div class="comment-box">
+        <div class="comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto temporibus iste nostrum dolorem natus recusandae incidunt voluptatum.</div>
+        <div class="comment-footer">
+          <div class="comment-info">
+            <span class="comment-author">
+              <a href="">Sexar</a>
+            </span>
+
+          </div>
+
+          <div class="comment-actions">
+            <a href="#">Reply</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+       <div class="comment">
+      <!-- Comment Avatar -->
+      <div class="comment-avatar">
+        <img src="http://gravatar.com/avatar/412c0b0ec99008245d902e6ed0b264ee?s=80">
+      </div>
+
+      <!-- Comment Box -->
+      <div class="comment-box">
+        <div class="comment-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto temporibus iste nostrum dolorem natus recusandae incidunt voluptatum.</div>
+        <div class="comment-footer">
+          <div class="comment-info">
+            <span class="comment-author">
+              <a href="">Sexar</a>
+            </span>
+
+          </div>
+
+          <div class="comment-actions">
+            <a href="#">Reply</a>
+          </div>
+        </div>
+      </div>
+    </div>
+      
+      
+    <!-- Comment - Dummy -->
+    <div class="comment">
+      <!-- Comment Avatar -->
+      <div class="comment-avatar">
+        <img src="http://lorempixel.com/200/200/people">
+      </div>
+
+      <!-- Comment Box -->
+      <div class="comment-box">
+        <div class="comment-text">Eligendi voluptatum ducimus architecto tempore, quaerat explicabo veniam fuga corporis totam reprehenderit quasi sapiente modi tempora at perspiciatis mollitia, dolores voluptate. Cumque, corrupti?</div>
+        <div class="comment-footer">
+          <div class="comment-info">
+            <span class="comment-author">
+              <a href="mailto:ximme13@somedomain.io">Ximme</a>
+            </span>
+         
+          </div>
+
+          <div class="comment-actions">
+            <a href="#">Reply</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+<!--Comment Section End-->
 
 	<!--Related Products-->
     {{-- <div class="container" style="background-color:#ffffff;">
