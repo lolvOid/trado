af2adfd514d90efc87de68d30014417ccb7414cd
unknown <stm1998sithumyo@gmail.com>
2018-09-02 23:37:01 +0630

    ''

--- vendor/tcg/voyager/resources/views/dashboard/navbar.blade.php
+++ vendor/tcg/voyager/resources/views/dashboard/navbar.blade.php
@@ -34,11 +34,11 @@
         <ul class="nav navbar-nav @if (config('voyager.multilingual.rtl')) navbar-left @else navbar-right @endif">
             <li class="dropdown profile">
                 <a href="#" class="dropdown-toggle text-right" data-toggle="dropdown" role="button"
-                   aria-expanded="false"><img src="{{ $user_avatar }}" class="profile-img"> <span
+                   aria-expanded="false"><img src="{{ Auth::user()->avatar }}" class="profile-img"> <span
                             class="caret"></span></a>
                 <ul class="dropdown-menu dropdown-menu-animated">
                     <li class="profile-img">
-                        <img src="{{ $user_avatar }}" class="profile-img">
+                        <img src="{{ Auth::user()->avatar }}" class="profile-img">
                         <div class="profile-body">
                             <h5>{{ Auth::user()->name }}</h5>
                             <h6>{{ Auth::user()->email }}</h6>
--- vendor/tcg/voyager/resources/views/dashboard/sidebar.blade.php
+++ vendor/tcg/voyager/resources/views/dashboard/sidebar.blade.php
@@ -19,7 +19,7 @@
                  style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'), voyager_asset('images/bg.jpg') ) }}); background-size: cover; background-position: 0px;">
                 <div class="dimmer"></div>
                 <div class="panel-content">
-                    <img src="{{ $user_avatar }}" class="avatar" alt="{{ Auth::user()->name }} avatar">
+                    <img src="{{Auth::user()->avatar }}" class="avatar" alt="{{ Auth::user()->name }} avatar">
                     <h4>{{ ucwords(Auth::user()->name) }}</h4>
                     <p>{{ Auth::user()->email }}</p>
 
