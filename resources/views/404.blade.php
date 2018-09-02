f0fa4ee1f4526b4910da88a0f5174809f9da3535
kiyokata <42716777+kiyokata@users.noreply.github.com>
2018-09-03 04:52:35 +0630

    Update 404.blade.php

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -10,16 +10,11 @@
      <hr>
      <div class='_1'>THE PAGE</div>
      <div class='_2'>WAS NOT FOUND</div>
-<<<<<<< HEAD
+
      <a class='btn' href='#'>BACK TO HOME</a>
  </div>
 
 </div>
 @endsection()
-=======
-     <a class='btn404' href='#'>BACK TO HOME</a>
- </div>
 
-</div>
-@endsection()
->>>>>>> 3f1e87068011d2af5b5a5a23ad3412a4f790abbe
+   
