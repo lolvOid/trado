e6419e7a68f14a4c0beef7bb53a3fd69586760a3
unknown <stm1998sithumyo@gmail.com>
2018-09-02 20:55:43 +0630

    ''

--- resources/views/partials/dashboardheader.blade.php
+++ resources/views/partials/dashboardheader.blade.php
@@ -236,7 +236,7 @@
                       <li><a href="{{route('wallet.show')}}" class="waves-effect waves-button"><span class="menu-icon icon-wallet"></span><p>Wallet</p></a></li>
                             <li><a href="{{route('statistics.index')}}" class="waves-effect waves-button"><span class="menu-icon icon-grid"></span><p>Statisics</p></a></li>
                         <li><a href="{{route('productdata.index')}}" class="waves-effect waves-button"><span class="menu-icon icon-basket"></span><p>Products</p></a></li>
-                            <li><a href="" class="waves-effect waves-button"><span class="menu-icon icon-power"></span><p>Logout</p></a></li>
+                        <li><a href="/logout" class="waves-effect waves-button"><span class="menu-icon icon-power"></span><p>Logout</p></a></li>
                         
                         </ul>
                     </div><!-- Page Sidebar Inner -->
