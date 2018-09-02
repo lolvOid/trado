af468819f138ea3fb7dd29a2959aae81aad93677
unknown <stm1998sithumyo@gmail.com>
2018-09-02 23:49:58 +0630

    ''

--- vendor/tcg/voyager/resources/views/dashboard/sidebar.blade.php
+++ vendor/tcg/voyager/resources/views/dashboard/sidebar.blade.php
@@ -4,12 +4,9 @@
             <div class="navbar-header">
                 <a class="navbar-brand" href="{{ route('voyager.dashboard') }}">
                     <div class="logo-icon-container">
-                        <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
-                        @if($admin_logo_img == '')
-                            <img src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
-                        @else
-                            <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
-                        @endif
+                    
+                        <img src="/img/tradolarge.png" alt="Logo Icon">
+                        
                     </div>
                     <div class="title">{{Voyager::setting('admin.title', 'VOYAGER')}}</div>
                 </a>
