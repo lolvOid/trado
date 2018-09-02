1108a31c08ffa618e61175f39b368b39ebf8f0d2
unknown <stm1998sithumyo@gmail.com>
2018-09-02 23:18:58 +0630

    ''

--- app/comment.php
+++ app/comment.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class comment extends Model
 {
     protected $fillable = [
-        'owner_id', 'user_id', 'comment','product_id'
+        'owner_id', 'user_id', 'comment','product_id', 'cmt_user_id'
     ];
 }
